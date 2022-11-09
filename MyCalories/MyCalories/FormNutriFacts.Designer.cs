fbb31c214cceb23ccf9cf5d0e6311146457f0f07
saddansyah <saddanakbar@gmail.com>
2022-11-09 07:07:56 +0700

    add user form

--- MyCalories/MyCalories/FormNutriFacts.Designer.cs
+++ MyCalories/MyCalories/FormNutriFacts.Designer.cs
@@ -69,7 +69,7 @@
             this.Controls.Add(this.label3);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormNutriFacts";
             this.Text = "FormNutriFacts";
             this.Load += new System.EventHandler(this.FormNutriFacts_Load);
--- MyCalories/MyCalories/FormUser.Designer.cs
+++ MyCalories/MyCalories/FormUser.Designer.cs
@@ -2,38 +2,32 @@
 {
     partial class FormUser
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormUser";
-        }
-
-        #endregion
+        private Label lblTitle2;
+        private DataGridView dgvData;
+        private Panel panelForm;
+        private Button button2;
+        private Button button1;
+        private Panel pnlRoles;
+        private TextBox tbRoles;
+        private Label label8;
+        private Panel pnlHealthStatus;
+        private TextBox tbHealthStatus;
+        private Label label7;
+        private Panel pnlWeight;
+        private TextBox tbWeight;
+        private Label label6;
+        private Panel pnlHeight;
+        private TextBox tbHeight;
+        private Label label5;
+        private Panel pnlGender;
+        private TextBox tbGender;
+        private Label label4;
+        private Panel pnlAge;
+        private TextBox tbAge;
+        private Label label3;
+        private Label label1;
+        private Panel pnlNama;
+        private TextBox tbNama;
+        private Label label2;
     }
 }
